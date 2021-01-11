62d005f12fbd5e0dde3820b4b9c31697f9d300d9
Ippei Matsubara <matz1ppei@gmail.com>
2021-01-11 14:31:03 +0900

    update conftest.py

--- README.md
+++ README.md
@@ -2,8 +2,23 @@
 
 DMM API Client for Python
 
+## Dependencies
+requests
+
+## Install
+```bash
+pip intsall dmm
+```
+
 ## Usage
 ```python
 from dmm import DMM
 
+dmm = DMM('<API_ID>', '<AFFILIATE_ID>')
+```
+
+## Function
+### get_floors
+```python
+floors = dmm.get_floors()
 ```
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,32 @@ import pytest
 from dmm import DMM
 
 
+def pytest_addoption(parser):
+    parser.addoption('--local', action='store_true',
+                     help='get environment variables by .env')
+    parser.addoption('--apiid', action='store',
+                     help='APIID: DMM API ID')
+    parser.addoption('--affiliateid', action='store',
+                     help='AFFILIATEID: DMM Affiliate ID')
+
+
 @pytest.fixture(scope='session')
-def client():
-    api_id = os.environ['API_ID']
-    affiliate_id = os.environ['AFFILIATE_ID']
+def local(pytestconfig):
+    return pytestconfig.option.local
+
+
+@pytest.fixture(scope='session')
+def apiid(pytestconfig):
+    return pytestconfig.option.apiid
+
+
+@pytest.fixture(scope='session')
+def affiliateid(pytestconfig):
+    return pytestconfig.option.affiliateid
+
+
+@pytest.fixture(scope='session')
+def client(local, apiid, affiliateid):
+    api_id = os.environ['API_ID'] if local else apiid
+    affiliate_id = os.environ['AFFILIATE_ID'] if local else affiliateid
     return DMM(api_id, affiliate_id)
